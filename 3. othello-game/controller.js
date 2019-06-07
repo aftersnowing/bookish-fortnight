77c1c9802b9d20043a75d4cb037a02c9c1d61b82
aftersnowing <gurcks2@gmail.com>
2019-06-07 18:42:44 +0900

    feat: 주석추가

--- 3. othello-game/controller.js
+++ 3. othello-game/controller.js
@@ -39,10 +39,12 @@ class Controller {
   getAllElements(arr) {
     let k = 0;
     while (k < this.directions.length) {
+      // 놓여지는 돌을 중심으로 8방향에 대해 반복문
       let container = [];
       let i = this.input[0];
       let j = this.input[1];
       while (i >= 0 && j >= 0 && i < 8 && j < 8) {
+        // 각 방향의 끝까지 탐색
         container.push({ value: model.playData[i][j], position: [i, j] });
         if (this.directions[k] === "w") j--;
         if (this.directions[k] === "nw") i++, j--;
@@ -67,8 +69,11 @@ class Controller {
     return arr.map(val => {
       let tempArr = [];
       val.some(el => {
+        // 빈자리가 있다면 바로 리턴
         if (el.value === 0) return true;
+        // 현재 턴과 같은 돌은 'player'로 변환
         if (el.value === model.player) tempArr.push("player");
+        // 현재 턴과 다른 돌은 좌표로 변환
         if (el.value !== model.player) tempArr.push(el.position);
       });
       return tempArr;
@@ -78,11 +83,14 @@ class Controller {
   getValidPostion(arr) {
     return arr.map(val => {
       let arr = [];
+      // 'player'가 존재한다면 toggle on
       let toggle = 0;
       val.forEach(el => {
+        // 'player'를 만날때까지 다른 돌의 좌표 push
         if (arr.indexOf("player") === -1) arr.push(el);
         if (el === "player") toggle = 1;
       });
+      // 'player'가 존재한다면 arr반환, 없다면 빈 배열 반환
       return toggle === 1 ? arr.slice(0, -1) : [];
     });
   }
