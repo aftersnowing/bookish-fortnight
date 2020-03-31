cd3ba04647ddbb3a0e763b86d1920bb31f229556
aftersnowing <gurcks2@gmail.com>
2020-03-31 23:54:28 +0900

    중력가속도 구현 완료

--- 7. gravityAcceleration/gravityAcceleration.js
+++ 7. gravityAcceleration/gravityAcceleration.js
@@ -1,73 +1,69 @@
-const g = 98;
-const height = 600;
-let intervalId = undefined;
-let unitTime = 0.015;
+const g = 9.8;
+const unitTime = 0.065;
+const elasticity = 0.7;
+
 let v = 0;
 let h = 600;
 let pixel = 0;
 let toggle = true;
-let dy = 0;
-let dy_2 = 0;
-let isDone = false;
+let intervalId = null;
 
-function getGravityAcceleration() {
+function startGravityAcceleration() {
   if (toggle) {
-    v += g * unitTime;
-    dy = (1 / (2 * g)) * (Math.pow(v, 2) - Math.pow(v - g * unitTime, 2));
-    dy_2 = ((2 * v - g * unitTime) * unitTime) / 2;
-    h = h - dy_2;
-    pixel = pixel + dy_2;
-    console.log("first", "dy:", dy, "dy_2:", dy_2, "pixel:", pixel);
-    renderDom("+");
-    checkReflection();
+    dropBall();
+    checkBounce();
   }
 
   if (!toggle) {
-    v -= g * unitTime;
-    dy = (1 / (2 * g)) * (Math.pow(v, 2) - Math.pow(v - g * unitTime, 2));
-    dy_2 = ((2 * v - g * unitTime) * unitTime) / 2;
-    h = h + dy_2;
-    console.log("second", "dy:", dy, "dy_2:", dy_2, "pixel", pixel);
-    pixel = pixel - dy_2;
-    renderDom();
-    checkHighestPosition();
+    bounceBall();
+    checkDrop();
   }
 }
 
-function syncronizePixel() {
-  pixel = dy_2;
+function dropBall() {
+  v += g * unitTime;
+  h = h - getDeltaY();
+  pixel = pixel + getDeltaY();
+  renderDom();
 }
 
-function checkReflection() {
+function bounceBall() {
+  v = v -  g * unitTime;
+  h = h + getDeltaY();
+  pixel = pixel - getDeltaY();
+  renderDom();
+}
+
+function getDeltaY() {
+  return (1 / (2 * g)) * (Math.pow(v, 2) - Math.pow(v - g * unitTime, 2));
+}
+
+function checkBounce() {
   if (pixel >= 600) {
     pixel = 600;
     h = 0;
-    v = 0.7 * v;
+    v = elasticity * v;
     renderDom();
     toggle = !toggle;
   }
 }
 
-function checkHighestPosition() {
+function checkDrop() {
   if (v <= 0) {
-    if (pixel > 599) {
-      v = 0;
-      h = 0;
-      pixel = 0;
-      renderDom();
-      return clearInterval(intervalId);
-    }
+    checkOnGround();
     toggle = !toggle;
   }
 }
 
-function runSetInterval(callback, time) {
-  intervalId = setInterval(callback, time);
-}
-
-function setPosition(pixel) {
-  document.getElementsByClassName("ball")[0].classList.add("top");
-  console.log(1);
+function checkOnGround() {
+  if (pixel > 599.5) {
+    v = 0;
+    h = 0;
+    pixel = 600;
+    renderDom();
+    clearInterval(intervalId);
+    intervalId = null;
+  }
 }
 
 function renderDom() {
@@ -78,9 +74,23 @@ function renderDom() {
   )[0].style.transform = `translateY(${pixel}px)`;
 }
 
-const button = document.getElementById("button");
-button.addEventListener(
+function runSetInterval(callback, time) {
+  if(!intervalId) intervalId = setInterval(callback, time);
+}
+
+function stopSetInterval() {
+  clearInterval(intervalId);
+  intervalId = null;
+}
+
+document.getElementById("button").addEventListener(
   "click",
-  runSetInterval.bind(null, getGravityAcceleration, 0),
+  runSetInterval.bind(null, startGravityAcceleration, 0),
+  false
+);
+
+document.getElementById("button_2").addEventListener(
+  "click",
+  stopSetInterval,
   false
 );
