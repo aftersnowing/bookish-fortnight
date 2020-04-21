ec6d133ad8f71bdbce156d6e6a75da9e58389f0c
KwonHyukChan <gurcks2@gmail.com>
2020-04-21 09:26:15 +0900

    HMAC 수정

--- 6. API authentication toy/client_side/HMAC_test.js
+++ 6. API authentication toy/client_side/HMAC_test.js
@@ -1,21 +1,32 @@
-function GenerateHMAC(sharedKey) {
-	// const timeStamp = Math.floor(new Date().getTime() / 1000).toString();
-	const timeStamp = "1234567890";
-	const encordedTimeStamp_base64 = btoa(timeStamp);
-	const hashCode = CryptoJS.HmacSHA256(encordedTimeStamp_base64, sharedKey);
-	const encordedHashCode_base64 = CryptoJS.enc.Base64.stringify(hashCode);
-	document.getElementById('hashCode').innerHTML = `hash code: ${encordedHashCode_base64}`;
-	document.getElementById('timeStamp').innerHTML = `time stamp: ${encordedTimeStamp_base64}`;
-	return {encordedHashCode_base64, timeStamp}
+let apiKeys = null;
+
+function generateHMAC(sharedKey) {
+  const timeStamp = Math.floor(new Date().getTime() / 1000).toString();
+  const encordedTimeStamp_base64 = btoa(timeStamp);
+  const hashCode = CryptoJS.HmacSHA256(encordedTimeStamp_base64, sharedKey);
+  const encordedHashCode_base64 = CryptoJS.enc.Base64.stringify(hashCode);
+  document.getElementById(
+    "hashCode"
+  ).innerHTML = `hash code: ${encordedHashCode_base64}`;
+  document.getElementById("timeStamp").innerHTML = `time stamp: ${timeStamp}`;
+  apiKeys = { encordedHashCode_base64, timeStamp };
 }
-const apiKeys = GenerateHMAC('POSSM');
 
 function httpGet(url) {
-    var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open( "GET", url, true);
-	// xmlHttp.setRequestHeader('Authentication', apiKeys.encordedHashCode_base64);
-	// xmlHttp.setRequestHeader('Time-Stamp', apiKeys.timeStamp);
-	xmlHttp.send(null);
-    return xmlHttp.responseText;
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", url, true);
+  xmlHttp.setRequestHeader("Authentication", apiKeys.encordedHashCode_base64);
+  xmlHttp.setRequestHeader("Time-Stamp", apiKeys.timeStamp);
+  xmlHttp.send(null);
+  return xmlHttp.responseText;
 }
-httpGet('http://127.0.0.1:57412/HMACTEST');
+
+generateHMAC("POSSM");
+httpGet("http://127.0.0.1:57412/HMACTEST");
+
+document
+  .getElementById("Renew")
+  .addEventListener("click", () => generateHMAC("POSSM"));
+document
+  .getElementById("Request")
+  .addEventListener("click", () => httpGet("http://127.0.0.1:57412/HMACTEST"));
